771c385a92840349b99ac4805a4d2206827fa054
Tiago <tiago.bittencourt.2005@gmail.com>
2025-02-02 09:35:42 -0300

    Add 'Medico' e 'Paciente' Feature to Clinica Class

---
+++ src/servicos/Clinica.java
@@ -0,0 +1,101 @@
+package servicos;
+
+import entidades.Medico;
+import entidades.Paciente;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Clinica {
+    private ArrayList<Paciente> paciente;
+    private ArrayList<Medico> medicos;
+
+    private static final DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
+    public static void adicionarPaciente() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o nome do paciente: "); // TODO: Nao Liberar nomes em branco
+        String nomePaciente = sc.nextLine();
+        System.out.println("Digite o cpf: "); // TODO: Aplicar um formatador (cpf)
+        String cpfPaciente = sc.nextLine();
+        Boolean exit = false;
+        while (!Paciente.isCpfValido(cpfPaciente)) {
+            System.out.println("Cpf ja cadastrado! Digite (s) para sair ou entre ou cpf valido");
+            cpfPaciente = sc.nextLine();
+            if (cpfPaciente.equalsIgnoreCase("s")) {
+                return;
+            }
+        }
+
+        LocalDate dataNascimentoPaciente = null;
+        while (dataNascimentoPaciente == null) {
+            try {
+                System.out.print("Digite a data de nascimento no formato (dd-mm-yyyy): ");
+                String input = sc.nextLine();
+
+                // Tenta converter a string para LocalDate
+                dataNascimentoPaciente = LocalDate.parse(input, formato);
+
+                // Checa se a data nao esta no futuro
+                if (dataNascimentoPaciente.isAfter(LocalDate.now())) {
+                    System.out.println("A data de nascimento nao pode estar no futuro.");
+                    dataNascimentoPaciente = null;
+                }
+            } catch (DateTimeParseException e) {
+                System.out.println("Formato invalido! Use o formato dd-mm-yyyy.");
+            }
+        }
+        Paciente p = new Paciente(nomePaciente, cpfPaciente, dataNascimentoPaciente);
+        p.displayDados();
+    }
+    public static void adicionarMedico() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o nome do medico: "); // TODO: Nao Liberar nomes em branco
+        String nomeMedico = sc.nextLine();
+        System.out.println("Digite o cpf: "); // TODO: Aplicar um formatador (cpf)
+        String cpfMedico = sc.nextLine();
+        Boolean exit = false;
+        while (!Paciente.isCpfValido(cpfMedico)) {
+            System.out.println("Cpf ja cadastrado! Digite (s) para sair ou entre ou cpf valido");
+            cpfMedico = sc.nextLine();
+            if (cpfMedico.equalsIgnoreCase("s")) {
+                return;
+            }
+        }
+
+        LocalDate dataNascimentoMedico = null;
+        while (dataNascimentoMedico == null) {
+            try {
+                System.out.print("Digite a data de nascimento no formato (dd-mm-yyyy): ");
+                String input = sc.nextLine();
+
+                // Tenta converter a string para LocalDate
+                dataNascimentoMedico = LocalDate.parse(input, formato);
+
+                // Checa se a data nao esta no futuro
+                if (dataNascimentoMedico.isAfter(LocalDate.now())) {
+                    System.out.println("A data de nascimento nao pode estar no futuro.");
+                    dataNascimentoMedico = null;
+                }
+            } catch (DateTimeParseException e) {
+                System.out.println("Formato invalido! Use o formato dd-mm-yyyy.");
+            }
+        }
+        Paciente p = new Paciente(nomeMedico, cpfMedico, dataNascimentoMedico);
+        p.displayDados();
+    }
+
+
+
+
+    public static void agendarConsulta(Integer idPaciente, Integer idMedico, LocalDate data, LocalTime hora, Integer duracao, Prescricao prescricao, BigDecimal valor ) {
+        int something = 10;
+
+    }
+}
