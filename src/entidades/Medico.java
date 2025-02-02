6ebcf2fcc0c1f910cebbb28237d963765cd8b032
Tiago <tiago.bittencourt.2005@gmail.com>
2025-02-01 21:01:31 -0300

    IDs and Display Data Implemetation

--- src/entidades/Medico.java
+++ src/entidades/Medico.java
@@ -5,11 +5,21 @@ import java.time.LocalDate;
 public class Medico extends Pessoa{
     private Integer crm;
     private String especialidade;
+
     public Medico(String nome, String cpf, LocalDate dataNascimento, Integer crm, String especialidade) {
         super(nome, cpf, dataNascimento);
         this.crm = crm;
         this.especialidade = especialidade;
     }
+
+    @Override
+    public void displayDados() {
+        super.displayDados();
+        System.out.println("Crm: " + this.crm);
+        System.out.println("Especialidade: " + this.especialidade);
+    }
+
+
     public Integer getCrm() {
         return crm;
     }
--- src/entidades/Pessoa.java
+++ src/entidades/Pessoa.java
@@ -4,57 +4,84 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 
 class Pessoa {
+    protected Integer id;
     protected String nome;
     protected String cpf;
     protected LocalDate dataNascimento;
-    protected ArrayList<String> historicoMedico; // TODO: "Consulta" Implementation
+    protected ArrayList<String> historicoMedico;
+
+    // Array de CPF registrado
     private static ArrayList<String> cpfsRegistrados = new ArrayList<>();
 
+    // Contador de ID
+    private static Integer contadorIds = 1;
+
     public Pessoa(String nome, String cpf, LocalDate dataNascimento) {
-        this.nome = nome;
-        // TODO: Better Error Handling and Better Error Customization
+
+        // Verifica se o CPF já foi registrado
         if (isCpfValido(cpf)) {
             this.cpf = cpf;
-            cpfsRegistrados.add(cpf); // Adicionando o CPF a lista de CPFs registrados
+            cpfsRegistrados.add(cpf); // Adiciona o CPF à lista de registrados
         } else {
-            System.out.println("CPF ja registrado");
+            System.out.println("CPF já registrado");
+            return; // Ou lançar uma exceção se preferir
         }
+
+        this.nome = nome;
         this.dataNascimento = dataNascimento;
-        historicoMedico = new ArrayList<>();
+        this.id = contadorIds++;  // ID unico para a instancia e aumenta o contatdor (+1) depois
+        this.historicoMedico = new ArrayList<>();
     }
 
-    // Checa se o cpf esta no Array statico da classe
+    public void displayDados() {
+        System.out.println("ID: " + this.id);
+        System.out.println("Nome: " + this.nome);
+        System.out.println("CPF: " + this.cpf);
+        System.out.println("Data de nascimento: " + this.dataNascimento);
+        System.out.println("Historico Medico: " + this.historicoMedico);
+    }
+
+    // Checa se o CPF ja esta registrado
     private boolean isCpfValido(String cpf) {
         return !cpfsRegistrados.contains(cpf);
     }
 
-
-
+    public Integer getId() {
+        return id;
+    }
 
     public String getNome() {
         return nome;
     }
+
     public void setNome(String nome) {
         this.nome = nome;
     }
+
     public String getCpf() {
         return cpf;
     }
+
     public void setCpf(String cpf) {
         this.cpf = cpf;
     }
+
     public LocalDate getDataNascimento() {
         return dataNascimento;
     }
+
     public void setDataNascimento(LocalDate dataNascimento) {
         this.dataNascimento = dataNascimento;
     }
+
     public ArrayList<String> getHistoricoMedico() {
         return historicoMedico;
     }
+
     public void adicionaHistoricoMedico(String historicoMedico) {
         this.historicoMedico.add(historicoMedico);
     }
+
     public void deletarHistoricoMedico(Integer i) {
         historicoMedico.remove(i);
     }
